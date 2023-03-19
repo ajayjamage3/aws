910e2d7c1510da38c5b3c281a63bd25a3a0d621f
Ajay Jamage <89625084+ajayjamage3@users.noreply.github.com>
2023-03-20 02:38:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const express = require("express")
 const app = express()
 app.use(express.json())
-const {connection} = require("./db")
-const {movieRouter} = require("./routes/movie.route")
-
-app.use("/movies",movieRouter)
 
 app.get("/",(req,res)=>{
-    res.send("Welcome")
+    res.send("Welcome to server 1")
 })
 
-app.listen(4500,async()=>{
-   try {
-    await connection
-    console.log("Connected to DB")
-   } catch (error) {
-    console.log("Error while connecting to db")
-    console.log(error)
-   }
+app.listen(4500,()=>{
    console.log(`Server running on port 4500`)
 })
